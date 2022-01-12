8139b7c372cbc4ec77777f37c8ccf1965de7f9da
Mihai Costea <macostea@users.noreply.github.com>
2022-01-12 19:46:15 +0200

    Use correct pkg command

--- root/bootstrap-radarr.sh
+++ root/bootstrap-radarr.sh
@@ -2,6 +2,6 @@
 
 cd /tmp
 curl -L -o radarr.txz "https://github.com/Thefrank/freebsd-port-sooners/releases/download/20210613/radarrv3-3.2.2.5080.txz"
-pkg -y radarr.txz
+pkg install -y radarr.txz
 
 rm -rf /tmp/radarr.txz
